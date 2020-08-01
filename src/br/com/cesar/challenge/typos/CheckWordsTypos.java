8398e283e12d35746ed3c9bade3fa081537a931a
Braulio Neto <bnetogil@gmail.com>
2020-08-01 18:12:03 -0400

    initial commit

---
+++ src/br/com/cesar/challenge/typos/CheckWordsTypos.java
@@ -0,0 +1,13 @@
+package br.com.cesar.challenge.typos;
+
+/**
+ * Class to check words typos
+ */
+public class CheckWordsTypos {
+
+    public boolean checkTypos(final String strOne, final String strTwo) {
+
+        return false;
+    }
+
+}
